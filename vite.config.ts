5de9237c4214ee910a5273084accb5483a0bbf8b
toshi.stewart <toshi@chrono24.com>
2022-07-20 09:34:31 +0200

    push

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "node:path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    emptyOutDir: true,
+    outDir: './docs/'
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
